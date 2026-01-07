ac77dae2d1f4fd0c7968856610ffaad6ff8bc1ea
clovnAnton <anton@byalko.ru>
2026-01-07 17:35:15 +0300

    Update profiles.js

--- js/data/profiles.js
+++ js/data/profiles.js
@@ -1,242 +1,197 @@
 export const dbProfiles = [
     {
-        "name": "Deadly_King82",
-        "img": "https://avatars.akamai.steamstatic.com/00d3d5f30cb755c3c06346b0a79051d9e2b175a0_full.jpg",
+        "name": "zlortilk",
+        "img": "https://i.pinimg.com/736x/f3/c7/9f/f3c79fe2279d87745206a27a612ba7e1.jpg",
         "game": "Dota 2",
+        "gender": "Male",
         "tags": [
-            "No Toxic"
+            "Discord"
         ],
-        "desc": "Turbo mode only, quick games."
+        "desc": "Roaming support, I help all lanes."
     },
     {
-        "name": "Ghost_Bear94",
-        "img": "https://avatars.akamai.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
-        "game": "Dota 2",
-        "tags": [
-            "Funny",
-            "Newbie",
-            "Mic On"
-        ],
-        "desc": "Turbo mode only, quick games."
-    },
-    {
-        "name": "Fast_Wolf5",
-        "img": "https://avatars.akamai.steamstatic.com/7a637a2f44604543d2c94314777d4c064ca06757_full.jpg",
+        "name": "HeadshotZorro",
+        "img": "https://i.pinimg.com/736x/b1/2a/85/b12a85728bcefcc75986ddee404d3a61.jpg",
         "game": "CS2",
+        "gender": "Male",
         "tags": [
-            "Chill",
-            "Mic On",
-            "No Toxic"
+            "Night Owl",
+            "Teamplay"
         ],
-        "desc": "Chill MM games, no toxic pls."
+        "desc": "Looking for a full stack for weekend tournaments."
     },
     {
-        "name": "Neon_Dragon34",
-        "img": "https://avatars.akamai.steamstatic.com/7a637a2f44604543d2c94314777d4c064ca06757_full.jpg",
-        "game": "Rust",
-        "tags": [
-            "Chill",
-            "No Toxic",
-            "Tryhard"
-        ],
-        "desc": "Duo looking for a third, 2k+ hours."
-    },
-    {
-        "name": "Crazy_Knight83",
-        "img": "https://avatars.akamai.steamstatic.com/97ce8528c11684c9873d61ba2660a9c687786401_full.jpg",
+        "name": "Zippityzoom",
+        "img": "https://avatars.mds.yandex.net/i?id=e58c8bd06ac55ee90d800a08f4fdbe86_l-4216502-images-thumbs&n=13",
         "game": "Deadlock",
+        "gender": "Male",
         "tags": [
-            "Chill",
-            "Funny",
-            "18+"
+            "Night Owl"
         ],
-        "desc": "High MMR player looking for team."
+        "desc": "Unranked only. Just having fun."
     },
     {
-        "name": "Lazy_Wolf95",
-        "img": "https://avatars.akamai.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
-        "game": "Deadlock",
-        "tags": [
-            "Funny"
-        ],
-        "desc": "Testing new builds, unranked."
-    },
-    {
-        "name": "Happy_Sniper32",
-        "img": "https://avatars.akamai.steamstatic.com/f1dd60a188883caf82ee00abd8a8bc1feb94e30b_full.jpg",
+        "name": "Klimixoox",
+        "img": "https://avatars.mds.yandex.net/i?id=1c77ba791a9afeb9235bff08c73230b3_l-8201030-images-thumbs&n=13",
         "game": "CS2",
-        "tags": [
-            "Tryhard"
-        ],
-        "desc": "Chill MM games, no toxic pls."
-    },
-    {
-        "name": "Dark_Knight86",
-        "img": "https://avatars.akamai.steamstatic.com/c5d56249ee5d28a07db4ac9f2f37fc4a205663a5_full.jpg",
-        "game": "CS2",
-        "tags": [
-            "No Toxic",
-            "Mic On",
-            "Funny"
-        ],
-        "desc": "Looking for a full stack for weekend cup."
-    },
-    {
-        "name": "Angry_King96",
-        "img": "https://avatars.akamai.steamstatic.com/5c982cb8a994775d7945d985a5392579b768c679_full.jpg",
-        "game": "CS2",
+        "gender": "Male",
         "tags": [
             "Pro",
-            "Newbie",
+            "Teamplay",
+            "Streamer"
+        ],
+        "desc": "Looking for a duo to vibe with. No toxic."
+    },
+    {
+        "name": "Coconutcaboodle",
+        "img": "https://i.pinimg.com/736x/4b/a3/a8/4ba3a895cfaf2ea28299131eb5452441.jpg",
+        "game": "Rust",
+        "gender": "Male",
+        "tags": [
+            "Support"
+        ],
+        "desc": "Base bitch looking for chads to protect me."
+    },
+    {
+        "name": "Kludgem0nster",
+        "img": "https://avatars.mds.yandex.net/i?id=4e446df953d8ad9e4217feb88ca2f931_l-5433602-images-thumbs&n=13",
+        "game": "Dota 2",
+        "gender": "Male",
+        "tags": [
+            "Voice Chat"
+        ],
+        "desc": "Pos 1 carry, looking for a support with a mic."
+    },
+    {
+        "name": "zlortilk",
+        "img": "https://avatars.mds.yandex.net/i?id=c78d7f04aef31c3842277fbc496f15ce_l-8342321-images-thumbs&n=13",
+        "game": "Deadlock",
+        "gender": "Male",
+        "tags": [
+            "Streamer"
+        ],
+        "desc": "I play for fun, rank doesn't matter."
+    },
+    {
+        "name": "klutzlepuff",
+        "img": "https://i.pinimg.com/originals/ed/26/39/ed2639ac5b26bff92bfa29a5fd8157bf.jpg?nii=t",
+        "game": "Dota 2",
+        "gender": "Male",
+        "tags": [
+            "Support",
+            "No Toxic"
+        ],
+        "desc": "Roaming support, I help all lanes."
+    },
+    {
+        "name": "HeadshotZorro",
+        "img": "https://i.pinimg.com/736x/f3/c7/9f/f3c79fe2279d87745206a27a612ba7e1.jpg",
+        "game": "Deadlock",
+        "gender": "Male",
+        "tags": [
+            "IGL",
+            "Voice Chat",
+            "No Tilt"
+        ],
+        "desc": "Looking for a duo to vibe with. No toxic."
+    },
+    {
+        "name": "Yumplejingle",
+        "img": "https://i.pinimg.com/originals/ed/26/39/ed2639ac5b26bff92bfa29a5fd8157bf.jpg?nii=t",
+        "game": "Deadlock",
+        "gender": "Male",
+        "tags": [
+            "Night Owl"
+        ],
+        "desc": "Main AWP looking for entry fraggers."
+    },
+    {
+        "name": "moggwixxo",
+        "img": "https://avatars.mds.yandex.net/i?id=1c77ba791a9afeb9235bff08c73230b3_l-8201030-images-thumbs&n=13",
+        "game": "Rust",
+        "gender": "Male",
+        "tags": [
+            "Streamer"
+        ],
+        "desc": "Beer and Dota on Friday nights. Join us!"
+    },
+    {
+        "name": "Yumplejingle",
+        "img": "https://i.pinimg.com/736x/b1/2a/85/b12a85728bcefcc75986ddee404d3a61.jpg",
+        "game": "Rust",
+        "gender": "Male",
+        "tags": [
+            "Chill",
+            "IGL",
+            "Strategy"
+        ],
+        "desc": "Beer and Dota on Friday nights. Join us!"
+    },
+    {
+        "name": "Coconutcaboodle",
+        "img": "https://i.pinimg.com/originals/82/5d/ce/825dce72cade4442b0741c81a4100a12.jpg",
+        "game": "Rust",
+        "gender": "Male",
+        "tags": [
+            "Pro",
             "18+"
         ],
-        "desc": "Global Elite smurfing, looking for duo."
+        "desc": "Grinding Faceit lvl 10. Need a duo."
     },
     {
-        "name": "Fast_Dragon86",
-        "img": "https://avatars.akamai.steamstatic.com/7a637a2f44604543d2c94314777d4c064ca06757_full.jpg",
-        "game": "CS2",
-        "tags": [
-            "Pro"
-        ],
-        "desc": "Need a reliable AWP player for premier."
-    },
-    {
-        "name": "Lazy_Knight70",
-        "img": "https://avatars.akamai.steamstatic.com/c5d56249ee5d28a07db4ac9f2f37fc4a205663a5_full.jpg",
-        "game": "Dota 2",
-        "tags": [
-            "No Toxic"
-        ],
-        "desc": "Mid laner looking for active supports."
-    },
-    {
-        "name": "Shadow_Boy1",
-        "img": "https://avatars.akamai.steamstatic.com/5c982cb8a994775d7945d985a5392579b768c679_full.jpg",
-        "game": "Rust",
-        "tags": [
-            "Newbie"
-        ],
-        "desc": "Duo looking for a third, 2k+ hours."
-    },
-    {
-        "name": "Deadly_Wolf25",
-        "img": "https://avatars.akamai.steamstatic.com/3932262d515a4521406825c88b9c6460010904d9_full.jpg",
+        "name": "zlortilk",
+        "img": "https://avatars.mds.yandex.net/i?id=4e446df953d8ad9e4217feb88ca2f931_l-5433602-images-thumbs&n=13",
         "game": "Deadlock",
+        "gender": "Male",
         "tags": [
-            "Chill",
-            "Mic On"
+            "Coach",
+            "Teamplay",
+            "Casual"
         ],
-        "desc": "Lane god looking for duo."
+        "desc": "Just looking for chill teammates for the evening."
     },
     {
-        "name": "Pro_Dragon90",
-        "img": "https://avatars.akamai.steamstatic.com/97ce8528c11684c9873d61ba2660a9c687786401_full.jpg",
+        "name": "Zippityzoom",
+        "img": "https://i.pinimg.com/originals/17/31/30/173130f14dd70db6429ecc8cec8c3295.jpg",
+        "game": "Deadlock",
+        "gender": "Male",
+        "tags": [
+            "18+",
+            "No Tilt",
+            "Cerry"
+        ],
+        "desc": "I play for fun, rank doesn't matter."
+    },
+    {
+        "name": "Frabblefrob",
+        "img": "https://avatars.mds.yandex.net/i?id=c78d7f04aef31c3842277fbc496f15ce_l-8342321-images-thumbs&n=13",
         "game": "Dota 2",
+        "gender": "Male",
         "tags": [
-            "Mic On",
-            "Pro"
+            "Coach"
         ],
-        "desc": "Mid laner looking for active supports."
+        "desc": "Builder looking for a PVP group in Rust."
     },
     {
-        "name": "Deadly_Player71",
-        "img": "https://avatars.akamai.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
-        "game": "CS2",
-        "tags": [
-            "Funny",
-            "Mic On",
-            "Pro"
-        ],
-        "desc": "Need a reliable AWP player for premier."
-    },
-    {
-        "name": "Holy_Wolf43",
-        "img": "https://avatars.akamai.steamstatic.com/c5d56249ee5d28a07db4ac9f2f37fc4a205663a5_full.jpg",
-        "game": "Dota 2",
-        "tags": [
-            "Mic On",
-            "Pro"
-        ],
-        "desc": "Turbo mode only, quick games."
-    },
-    {
-        "name": "Neon_Slayer81",
-        "img": "https://avatars.akamai.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
+        "name": "Yumplejingle",
+        "img": "https://avatars.mds.yandex.net/i?id=ed0c5d3f44084666495af3b2d0126aa3_l-5569020-images-thumbs&n=13",
         "game": "Rust",
+        "gender": "Male",
         "tags": [
-            "Funny",
-            "No Toxic"
-        ],
-        "desc": "PVP chad looking for snowball."
-    },
-    {
-        "name": "Fast_Ninja52",
-        "img": "https://avatars.akamai.steamstatic.com/3932262d515a4521406825c88b9c6460010904d9_full.jpg",
-        "game": "Dota 2",
-        "tags": [
-            "Newbie",
-            "Funny"
-        ],
-        "desc": "Grinding MMR, serious plays only."
-    },
-    {
-        "name": "Neon_King67",
-        "img": "https://avatars.akamai.steamstatic.com/00d3d5f30cb755c3c06346b0a79051d9e2b175a0_full.jpg",
-        "game": "CS2",
-        "tags": [
-            "Chill",
+            "Weekend Only",
             "Pro",
-            "Mic On"
+            "Casual"
         ],
-        "desc": "LFG Faceit lvl 10 grind."
+        "desc": "IGL looking for obedient teammates."
     },
     {
-        "name": "Pro_Wizard56",
-        "img": "https://avatars.akamai.steamstatic.com/7a637a2f44604543d2c94314777d4c064ca06757_full.jpg",
-        "game": "CS2",
+        "name": "Frabblefrob",
+        "img": "https://avatars.mds.yandex.net/i?id=1c77ba791a9afeb9235bff08c73230b3_l-8201030-images-thumbs&n=13",
+        "game": "Deadlock",
+        "gender": "Male",
         "tags": [
-            "Mic On",
-            "Tryhard"
+            "Chill"
         ],
-        "desc": "Global Elite smurfing, looking for duo."
-    },
-    {
-        "name": "Dark_Viper5",
-        "img": "https://avatars.akamai.steamstatic.com/3932262d515a4521406825c88b9c6460010904d9_full.jpg",
-        "game": "CS2",
-        "tags": [
-            "Newbie"
-        ],
-        "desc": "Chill MM games, no toxic pls."
-    },
-    {
-        "name": "Golden_Panda45",
-        "img": "https://avatars.akamai.steamstatic.com/c5d56249ee5d28a07db4ac9f2f37fc4a205663a5_full.jpg",
-        "game": "Dota 2",
-        "tags": [
-            "Mic On"
-        ],
-        "desc": "Grinding MMR, serious plays only."
-    },
-    {
-        "name": "Crazy_Bear98",
-        "img": "https://avatars.akamai.steamstatic.com/7a637a2f44604543d2c94314777d4c064ca06757_full.jpg",
-        "game": "Dota 2",
-        "tags": [
-            "Tryhard",
-            "Mic On"
-        ],
-        "desc": "Voice chat is a must."
-    },
-    {
-        "name": "Angry_Slayer96",
-        "img": "https://avatars.akamai.steamstatic.com/7a637a2f44604543d2c94314777d4c064ca06757_full.jpg",
-        "game": "Rust",
-        "tags": [
-            "Mic On",
-            "No Toxic"
-        ],
-        "desc": "Builder looking for a clan."
+        "desc": "Pos 1 carry, looking for a support with a mic."
     }
 ];
